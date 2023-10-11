97b78c1c92bb2f870fb14c4dbea95a7cc69d287c
Muhammad Hidayatullah <muhammadhidayatullah@MacBook-Air-Muhammad.local>
2023-10-11 15:49:55 +0800

    Ubah Title & Nyalakan Seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -14,27 +14,27 @@ class UsersSeeder extends Seeder
     public function run(): void
     {
         $userData = [
-            // [
-            //     'nama' => 'admin',
-            //     'email' => 'admin@gmail.com',
-            //     'password' => bcrypt('admin'),
-            //     'phoneNumber' => '087805982176',
-            //     'role' => 'admin'
-            // ],
-            // [
-            //     'nama' => 'usertes',
-            //     'email' => 'user@gmail.com',
-            //     'password' => bcrypt('user'),
-            //     'phoneNumber' => '081234567890',
-            //     'role' => 'user'
-            // ],
-            // [
-            //     'nama' => 'userdua',
-            //     'email' => 'user2@gmail.com',
-            //     'password' => bcrypt('user2'),
-            //     'phoneNumber' => '08123456830',
-            //     'role' => 'user'
-            // ],
+            [
+                'nama' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('admin'),
+                'phoneNumber' => '087805982176',
+                'role' => 'admin'
+            ],
+            [
+                'nama' => 'usertes',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('user'),
+                'phoneNumber' => '081234567890',
+                'role' => 'user'
+            ],
+            [
+                'nama' => 'userdua',
+                'email' => 'user2@gmail.com',
+                'password' => bcrypt('user2'),
+                'phoneNumber' => '08123456830',
+                'role' => 'user'
+            ],
             [
                 'nama' => 'GK402023001',
                 'email' => 'GK402023001',
